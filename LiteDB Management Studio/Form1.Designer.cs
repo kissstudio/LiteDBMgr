9571daa9ffd8f4672294d4938a3fa28ba0088693
tiny <tiny@sandsea.info>
2015-10-08 11:00:20 +0800

    project init

---
+++ LiteDB Management Studio/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace LiteDB_Management_Studio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(893, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load New";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 41);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(960, 560);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(952, 534);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(952, 534);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(713, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(812, 11);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 3;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(731, 11);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 613);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "LiteDB Management Studio";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ LiteDB Management Studio/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LiteDB;
+namespace LiteDB_Management_Studio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.buttonLoad.Enabled = false;
+            comboBox1.SelectedValueChanged += ComboBox1_SelectedValueChanged;
+            this.FormClosing += Form1_FormClosing;
+            LiteDatabase db = new LiteDatabase(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "app.bin"));
+            var x = db.GetCollection<Histories>(nameof(Histories));
+            if (x.Count() > 0)
+            {
+                var item = x.FindOne(Query.All());
+                this.comboBox1.Items.AddRange(item.Value);
+                if (item.Value.Any()) {
+                    this.comboBox1.SelectedItem = item.Value[0];
+                }
+                this.button2.Enabled = item.Value.Any();
+            }
+        }
+
+        private void ComboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            this.buttonLoad.Enabled = this.comboBox1.Items.Count > 0;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //saveHistory();
+        }
+
+        public class Histories
+        {
+            public string[] Value { get; set; }
+        }
+
+        void LoadDB(string fileName) {
+            LiteDatabase db = new LiteDatabase(fileName);
+            var cols = db.GetCollectionNames();
+            Text = fileName;
+            tabControl1.TabPages.Clear();
+            foreach (var colName in cols)
+            {
+                var tabPage = new TabPage(colName);
+                var tb = new DataTable();
+                foreach (var doc in db.GetCollection(colName).FindAll())
+                {
+                    if (tb.Columns.Count == 0)
+                        tb.Columns.AddRange(doc.Keys.Select(key => new DataColumn(key) { Caption = key }).ToArray());
+                    var row = tb.NewRow();
+                    foreach (var item in doc.Keys)
+                    {
+                        row[item] = doc[item];
+                    }
+                    tb.Rows.Add(row);
+                }
+                var dg = new DataGridView();
+                dg.DataSource = tb;
+                dg.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                dg.Dock = DockStyle.Fill;
+                tabPage.Controls.Add(dg);
+                tabControl1.TabPages.Add(tabPage);
+            }
+            foreach (var item in comboBox1.Items)
+            {
+                if (item.ToString() == fileName)
+                    return;
+            }
+            comboBox1.Items.Add(fileName);
+            saveHistory();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog f = new OpenFileDialog();
+            var x = f.ShowDialog(this);
+            if (x == DialogResult.OK)
+            {
+                try
+                {
+                    LoadDB(f.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.StackTrace, ex.Message);
+                }
+            }
+        }
+
+        private void saveHistory()
+        {
+            LiteDatabase db = new LiteDatabase(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "app.bin"));
+            var x = db.GetCollection<Histories>(nameof(Histories));
+            x.Delete(Query.All());
+            var items = new List<string>();
+            foreach (var item in comboBox1.Items)
+            {
+                items.Add(item.ToString());
+            }
+            x.Insert(new Histories()
+            {
+                Value = items.ToArray()
+            });
+        }
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null) {
+                LoadDB(comboBox1.SelectedItem.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LiteDatabase db = new LiteDatabase(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "app.bin"));
+            var x = db.GetCollection<Histories>(nameof(Histories));
+            x.Delete(Query.All());
+            comboBox1.Items.Clear();
+            this.buttonLoad.Enabled = false;
+        }
+    }
+}
